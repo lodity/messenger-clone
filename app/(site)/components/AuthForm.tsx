5f637239dcee0873c81d323185cc6076d8b27c4e
lodity <lodity9@gmail.com>
2023-08-07 09:27:46 +0300

    Register functionality

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "@/app/(site)/components/AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
 
 type variant = "LOGIN" | "REGISTER";
 
@@ -33,7 +34,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      // Axios Register
+      axios.post("/api/register", data);
     }
     if (variant === "LOGIN") {
       // NextAuth SignIn
