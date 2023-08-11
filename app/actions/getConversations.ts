787418dfb060a6133f1d3d67c5e46ffbf0f4eba9
lodity <lodity9@gmail.com>
2023-08-11 18:39:14 +0300

    Added getConversations action

---
+++ app/actions/getConversations.ts
@@ -0,0 +1,31 @@
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+const getConversations = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser?.id) {
+    return [];
+  }
+
+  try {
+    return await prisma.conversation.findMany({
+      orderBy: {
+        lastMessageAt: "desc",
+      },
+      where: {
+        userIds: {
+          has: currentUser.id,
+        },
+      },
+      include: {
+        users: true,
+        messages: { include: { sender: true, seen: true } },
+      },
+    });
+  } catch (e: any) {
+    return [];
+  }
+};
+
+export default getConversations;
